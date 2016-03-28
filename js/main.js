bc5b94ad755c0509f7676bd024ff9972a69de0de
KateAlf <katrin.alf@gmail.com>
2016-03-28 17:31:55 +0300

    Test for Icons

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ var nav = document.querySelector(".top-nav");
 
 menu.addEventListener("click", function (event) {
   event.preventDefault();
-  nav.classList.add("top-nav-show");
-  menu.classList.add("active");
+  nav.classList.toggle("top-nav-show");
+  menu.classList.toggle("active");
 });
 
-window.addEventListener("keydown", function (event) {
-  if (event.keyCode == 27) {
-    if (nav.classList.contains("top-nav-show")) {
-      nav.classList.remove("top-nav-show");
-      menu.classList.remove("active");
-    }
-  }
-});
+//window.addEventListener("keydown", function (event) {
+//  if (event.keyCode == 27) {
+//    if (nav.classList.contains("top-nav-show")) {
+//      nav.classList.remove("top-nav-show");
+//      menu.classList.remove("active");
+//    }
+//  }
+//});
